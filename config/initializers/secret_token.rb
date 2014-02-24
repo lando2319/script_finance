e6f68cc7c44f27d8c5dc1e62f46e70a0308cfc60
Mike Land <mikepland@gmail.com>
2014-02-24 17:33:45 -0600

    creates rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScriptFinance::Application.config.secret_token = 'd75b917e311ab5cff88f634f2810fcb7afb6a9d03525b5e9c4027a5c2b2135c136f8e3f76e33d92bf9a2c0aa6ace174c2cbe26c77afef58a7e2ff14c0ffe01f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ScriptFinance::Application.config.session_store :cookie_store, key: '_script_finance_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ScriptFinance::Application.config.session_store :active_record_store
